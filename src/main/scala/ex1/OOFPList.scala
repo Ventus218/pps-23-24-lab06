652234823521fa1f28929a51955efec77ab1a65a
Alessandro Venturini <ales.ventus21@gmail.com>
2024-04-15 10:48:13 +0200

    ex1 length done

--- src/main/scala/ex1/OOFPList.scala
+++ src/main/scala/ex1/OOFPList.scala
@@ -47,7 +47,8 @@ enum List[A]:
   // Exercise: implement the following methods
   def zipWithValue[B](value: B): List[(A, B)] = this.map((_, value))
 
-  def length(): Int = ???
+  def length(): Int = foldLeft(0)((l, _) => l + 1)
+    
   def zipWithIndex: List[(A, Int)] = ???
   def partition(predicate: A => Boolean): (List[A], List[A]) = ???
   def span(predicate: A => Boolean): (List[A], List[A]) = ???
@@ -69,6 +70,7 @@ object Test extends App:
   import List.*
   val reference = List(1, 2, 3, 4)
   println(reference.zipWithValue(10) == List((1, 10), (2, 10), (3, 10), (4, 10)))
+  println(reference.length() == 4)
   println(reference.zipWithIndex == List((1, 0), (2, 1), (3, 2), (4, 3)))
   println(reference.partition(_ % 2 == 0) == (List(2, 4), List(1, 3)))
   println(reference.span(_ % 2 != 0) == (List(1), List(2, 3, 4)))
