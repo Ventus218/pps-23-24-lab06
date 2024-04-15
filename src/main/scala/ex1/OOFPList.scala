e3a595080ef153c29937f156bc6da48973275190
Alessandro Venturini <ales.ventus21@gmail.com>
2024-04-15 10:44:15 +0200

    changed tests to output true if passed

--- src/main/scala/ex1/OOFPList.scala
+++ src/main/scala/ex1/OOFPList.scala
@@ -67,12 +67,12 @@ object Test extends App:
 
   import List.*
   val reference = List(1, 2, 3, 4)
-  println(reference.zipWithValue(10)) // List((1, 10), (2, 10), (3, 10), (4, 10))
-  println(reference.zipWithIndex) // List((1, 0), (2, 1), (3, 2), (4, 3))
-  println(reference.partition(_ % 2 == 0)) // (List(2, 4), List(1, 3))
-  println(reference.span(_ % 2 != 0)) // (List(1), List(2, 3, 4))
-  println(reference.span(_ < 3)) // (List(1, 2), List(3, 4))
-  println(reference.reduce(_ + _)) // 10
-  println(List(10).reduce(_ + _)) // 10
-  println(reference.takeRight(3)) // List(2, 3, 4)
-  println(reference.collect { case x if x % 2 == 0 => x + 1 }) // List(3, 5)
+  println(reference.zipWithValue(10) == List((1, 10), (2, 10), (3, 10), (4, 10)))
+  println(reference.zipWithIndex == List((1, 0), (2, 1), (3, 2), (4, 3)))
+  println(reference.partition(_ % 2 == 0) == (List(2, 4), List(1, 3)))
+  println(reference.span(_ % 2 != 0) == (List(1), List(2, 3, 4)))
+  println(reference.span(_ < 3) == (List(1, 2), List(3, 4)))
+  println(reference.reduce(_ + _) == 10 )
+  println(List(10).reduce(_ + _) == 10)
+  println(reference.takeRight(3) == List(2, 3, 4))
+  println(reference.collect { case x if x % 2 == 0 => x + 1 } == List(3, 5))
