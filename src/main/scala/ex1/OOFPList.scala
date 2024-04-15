f1347b4620c03fd4d1c5aec87e1ee18dcc987a5f
Alessandro Venturini <ales.ventus21@gmail.com>
2024-04-15 10:44:21 +0200

    ex 1 done

--- src/main/scala/ex1/OOFPList.scala
+++ src/main/scala/ex1/OOFPList.scala
@@ -45,7 +45,8 @@ enum List[A]:
     case h :: t => t.foldLeft(h)(op)
 
   // Exercise: implement the following methods
-  def zipWithValue[B](value: B): List[(A, B)] = ???
+  def zipWithValue[B](value: B): List[(A, B)] = this.map((_, value))
+
   def length(): Int = ???
   def zipWithIndex: List[(A, Int)] = ???
   def partition(predicate: A => Boolean): (List[A], List[A]) = ???
